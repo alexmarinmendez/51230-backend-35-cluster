49cb2d1c6ce0647658234f8ded75dc7d4953e1f9
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-07-26 08:42:22 -0500

    feat: creating one worker per cpu

--- src/app.js
+++ src/app.js
@@ -1,17 +1,14 @@
 import cluster from 'cluster'
+import { cpus } from 'os'
 
 if (cluster.isPrimary) {
-    console.log('Proceso primary trabajando...')
-    cluster.fork()
-    let result = 0
-    for (let index = 0; index < 10; index++) {
-        result += index
+    for (let index = 0; index < cpus().length; index++) {
+        cluster.fork()
     }
-    console.log(`Proceso primary (${process.pid}): ${result}`)
 } else {
     console.log('Proceso worker creado...')
     let result = 0
-    for (let index = 0; index < 5e9; index++) {
+    for (let index = 0; index < 5e3; index++) {
         result += index
     }
     console.log(`Proceso worker (${process.pid}): ${result}`)
